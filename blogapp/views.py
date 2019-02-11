f29d7a1525de07db0fcac240945c9d343db170bb
zahid-rahman <rahmanzahid94@gmail.com>
2019-02-12 00:35:56 +0600

    adding all post segment

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
 
 
 def front_view(request):
-    return render(request,'index.html')
+    all_post = Post.objects.all()
+    context = {
+        'all_post': all_post
+    }
+    return render(request, 'index.html', context)
+
 
 # def login_view(request):
 #     return render(request,'auth/login.html',{'title':'Login'})
 
 
-
 def about_view(request):
-    return render(request,'about/about.html',{'title':'About'})
+    return render(request, 'about/about.html', {'title': 'About'})
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,5 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from user import views as user_views
 from django.contrib.auth import views as auth_view
 from django.conf import settings
@@ -14,10 +13,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
-    path('profile/',user_views.profile_view,name='profile'),
-    path('register/',user_views.register_view,name='register'),
-    path('login/',auth_view.LoginView.as_view(template_name='auth/login.html'),name='login'),
-    path('logout/',auth_view.LogoutView.as_view(template_name='auth/logout.html'),name='logout'),
+    path('profile/', user_views.profile_view, name='profile'),
+    path('register/', user_views.register_view, name='register'),
+    path('login/', auth_view.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
 
     path('upload_post/', user_views.upload_post_view, name='upload.post.page'),
     path('user_post/<int:id>', user_views.view_user_post, name='user.post'),
